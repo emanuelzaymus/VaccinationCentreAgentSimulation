d5da306ba3d32339a0258a648c0d6dfd9a00bf2f
Emanuel Zaymus <emanuel.zaymus@prosoft.sk>
2021-05-03 23:32:16 +0200

    WaitingProcess with ancestor VaccinationCentreProcess

--- src/main/kotlin/sk/emanuelzaymus/agentsimulation/vaccinationcentre/waiting/WaitingProcess.kt
+++ src/main/kotlin/sk/emanuelzaymus/agentsimulation/vaccinationcentre/waiting/WaitingProcess.kt
@@ -1,37 +1,23 @@
 package sk.emanuelzaymus.agentsimulation.vaccinationcentre.waiting
 
-import OSPABA.*
+import OSPABA.CommonAgent
+import OSPABA.Simulation
 import OSPRNG.UniformContinuousRNG
-import sk.emanuelzaymus.agentsimulation.vaccinationcentre.debug
 import sk.emanuelzaymus.agentsimulation.vaccinationcentre.*
+import sk.emanuelzaymus.agentsimulation.vaccinationcentre.abstraction.VaccinationCentreProcess
 
 class WaitingProcess(mySim: Simulation, myAgent: CommonAgent) :
-    Process(Ids.waitingProcess, mySim, myAgent) {
+    VaccinationCentreProcess<Message>(Ids.waitingProcess, mySim, myAgent) {
 
     companion object {
         private val waitingDuration = UniformContinuousRNG(.0, 1.0)
     }
 
-    override fun processMessage(message: MessageForm) {
-        debug("WaitingProcess", message)
+    override val debugName = "WaitingProcess"
+    override val processEndMsgCode = MessageCodes.waitingEnd
 
-        when (message.code()) {
-            // WaitingManager - start waiting
-            IdList.start -> startActivity(message)
-
-            MessageCodes.waitingEnd -> endActivity(message)
-        }
-    }
-
-    private fun startActivity(message: MessageForm) {
-        message.setCode(MessageCodes.waitingEnd)
-
-        if (waitingDuration.sample() < WAITING_LESS_PROBABILITY)
-            hold(WAITING_DURATION_LESS, message)
-        else
-            hold(WAITING_DURATION_MORE, message)
-    }
-
-    private fun endActivity(message: MessageForm) = assistantFinished(message)
+    override fun getDuration(): Double =
+        if (waitingDuration.sample() < WAITING_LESS_PROBABILITY) WAITING_DURATION_LESS
+        else WAITING_DURATION_MORE
 
 }
