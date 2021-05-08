5b8c08c4eb9e8fdd4818335089e9a245f0339c93
Emanuel Zaymus <emanuel.zaymus@prosoft.sk>
2021-05-09 00:27:25 +0200

    C removed

--- src/main/kotlin/sk/emanuelzaymus/agentsimulation/vaccinationcentre/constants/Constants.kt
+++ src/main/kotlin/sk/emanuelzaymus/agentsimulation/vaccinationcentre/constants/Constants.kt
@@ -2,50 +2,51 @@ package sk.emanuelzaymus.agentsimulation.vaccinationcentre.constants
 
 // All time values are in seconds.
 
-internal const val WORKING_TIME: Double = 9 * 60 * 60.0 // 32_400
 private const val START_HOUR: Int = 8
 
-internal const val NORMAL_PATIENT_COUNT: Int = 540
-internal const val NOT_ARRIVING_PATIENTS_MIN: Double = 5.0
-internal const val NOT_ARRIVING_PATIENTS_MAX: Double = 25.0
+const val WORKING_TIME: Double = 9 * 60 * 60.0 // 32_400
 
-internal const val REGISTRATION_DURATION_MIN: Double = 140.0
-internal const val REGISTRATION_DURATION_MAX: Double = 220.0
+const val NORMAL_PATIENT_COUNT: Int = 540
+const val NOT_ARRIVING_PATIENTS_MIN: Double = 5.0
+const val NOT_ARRIVING_PATIENTS_MAX: Double = 25.0
 
-internal const val EXAMINATION_DURATION_MEAN: Double = 260.0
+const val REGISTRATION_DURATION_MIN: Double = 140.0
+const val REGISTRATION_DURATION_MAX: Double = 220.0
 
-internal const val VACCINATION_DURATION_MIN: Double = 20.0
-internal const val VACCINATION_DURATION_MODE: Double = 75.0
-internal const val VACCINATION_DURATION_MAX: Double = 100.0
+const val EXAMINATION_DURATION_MEAN: Double = 260.0
 
-internal const val WAITING_LESS_PROBABILITY: Double = 0.95
-internal const val WAITING_DURATION_LESS: Double = 15 * 60.0 // 900
-internal const val WAITING_DURATION_MORE: Double = 30 * 60.0 // 1_800
+const val VACCINATION_DURATION_MIN: Double = 20.0
+const val VACCINATION_DURATION_MODE: Double = 75.0
+const val VACCINATION_DURATION_MAX: Double = 100.0
 
-internal const val INJECTION_PREP_DURATION_MIN: Double = 6.0
-internal const val INJECTION_PREP_DURATION_MODE: Double = 10.0
-internal const val INJECTION_PREP_DURATION_MAX: Double = 40.0
-internal const val INJECTIONS_COUNT_TO_PREPARE: Int = 20
+const val WAITING_LESS_PROBABILITY: Double = 0.95
+const val WAITING_DURATION_LESS: Double = 15 * 60.0 // 900
+const val WAITING_DURATION_MORE: Double = 30 * 60.0 // 1_800
 
-internal const val EXAMINATION_TRANSFER_DURATION_MIN: Double = 40.0
-internal const val EXAMINATION_TRANSFER_DURATION_MAX: Double = 90.0
+const val INJECTION_PREP_DURATION_MIN: Double = 6.0
+const val INJECTION_PREP_DURATION_MODE: Double = 10.0
+const val INJECTION_PREP_DURATION_MAX: Double = 40.0
+const val INJECTIONS_COUNT_TO_PREPARE: Int = 20
 
-internal const val VACCINATION_TRANSFER_DURATION_MIN: Double = 20.0
-internal const val VACCINATION_TRANSFER_DURATION_MAX: Double = 45.0
+const val EXAMINATION_TRANSFER_DURATION_MIN: Double = 40.0
+const val EXAMINATION_TRANSFER_DURATION_MAX: Double = 90.0
 
-internal const val WAITING_TRANSFER_DURATION_MIN: Double = 45.0
-internal const val WAITING_TRANSFER_DURATION_MAX: Double = 110.0
+const val VACCINATION_TRANSFER_DURATION_MIN: Double = 20.0
+const val VACCINATION_TRANSFER_DURATION_MAX: Double = 45.0
 
-internal const val INJECTIONS_TRANSFER_DURATION_MIN: Double = 10.0
-internal const val INJECTIONS_TRANSFER_DURATION_MAX: Double = 18.0
+const val WAITING_TRANSFER_DURATION_MIN: Double = 45.0
+const val WAITING_TRANSFER_DURATION_MAX: Double = 110.0
 
-internal const val CANTEEN_TRANSFER_DURATION_MIN: Double = 70.0
-internal const val CANTEEN_TRANSFER_DURATION_MAX: Double = 200.0
+const val INJECTIONS_TRANSFER_DURATION_MIN: Double = 10.0
+const val INJECTIONS_TRANSFER_DURATION_MAX: Double = 18.0
 
-internal const val LUNCH_DURATION_MIN: Double = 5 * 60.0 // 300
-internal const val LUNCH_DURATION_MODE: Double = 15 * 60.0 // 900
-internal const val LUNCH_DURATION_MAX: Double = 30 * 60.0 // 1_800
+const val CANTEEN_TRANSFER_DURATION_MIN: Double = 70.0
+const val CANTEEN_TRANSFER_DURATION_MAX: Double = 200.0
 
-internal const val ADMIN_WORKERS_LUNCH_BREAK_START: Double = (11 - START_HOUR) * 60 * 60.0 // 10_800 // 11:00
-internal const val DOCTORS_LUNCH_BREAK_START: Double = (11 - START_HOUR) * 60 * 60 + 45 * 60.0 // 13_500 // 11:45
-internal const val NURSES_LUNCH_BREAK_START: Double = (13 - START_HOUR) * 60 * 60 + 30 * 60.0  // 19_800 // 13:30
+const val LUNCH_DURATION_MIN: Double = 5 * 60.0 // 300
+const val LUNCH_DURATION_MODE: Double = 15 * 60.0 // 900
+const val LUNCH_DURATION_MAX: Double = 30 * 60.0 // 1_800
+
+const val ADMIN_WORKERS_LUNCH_BREAK_START: Double = (11 - START_HOUR) * 60 * 60.0 // 10_800 // 11:00
+const val DOCTORS_LUNCH_BREAK_START: Double = (11 - START_HOUR) * 60 * 60 + 45 * 60.0 // 13_500 // 11:45
+const val NURSES_LUNCH_BREAK_START: Double = (13 - START_HOUR) * 60 * 60 + 30 * 60.0  // 19_800 // 13:30
